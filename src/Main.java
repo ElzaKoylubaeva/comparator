127fedf0139b77b7fa0f465e9b88d0fa43cba21c
Elza Koylubaeva <zakirova_elza@mail.ru>
2022-09-02 18:42:33 +0500

    Class NobilityComparator was removed. Instead of it was used lambda expression to execute the same logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -14,11 +15,27 @@ public class Main {
         people.add(new Person("Katie Scarlett", "OHara Hamilton Kennedy Butler", 100));
         people.add(new Person("Albus Percival", "Wulfric Brian Dumbledore", 99));
 
-        Collections.sort(people, new NobilityComparator(3));
+        Collections.sort(people, (o1, o2) -> {
+            String regex = "\\P{IsAlphabetic}+";
+            String[] isSplitSurname1 = o1.getSurname().split(regex);
+            String[] isSplitSurname2 = o2.getSurname().split(regex);
+            int maxNumberOfWordsInSurname = 3;
+            if (isSplitSurname1.length > maxNumberOfWordsInSurname
+                    && isSplitSurname2.length > maxNumberOfWordsInSurname) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            } else {
+                if (isSplitSurname1.length > isSplitSurname2.length) {
+                    return 1;
+                } else if (isSplitSurname1.length < isSplitSurname2.length) {
+                    return -1;
+                } else {
+                    return Integer.compare(o1.getAge(), o2.getAge());
+                }
+            }
+        });
 
         for (Person person : people) {
             System.out.println(person);
         }
-
     }
 }
